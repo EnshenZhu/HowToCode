26b396fd2225a578963483c968300f2d1fe94c6b
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-11-13 22:27:08 -0500

    learn the fundamental of mst

--- MinimumSpanningTree/script0.py
+++ MinimumSpanningTree/script0.py
@@ -24,13 +24,18 @@ with open(r"Prime_MST_input.txt") as f:
 
     for line in f.readlines():
         edge = tuple(map(int, line.split(" ")))
+
         if edge[0] == edge[1]: continue  # the edge is self looped
+
         heapq.heappush(all_edges[edge[0]], (edge[2], edge[1]))
+        # print(all_edges)
+
         heapq.heappush(all_edges[edge[1]], (edge[2], edge[0]))
-        # print(edges)
+
+    print(all_edges)
 
 cost, dest = 0, 1
-while len(usedVertices) < numVertices:
+while len(usedVertices) < numVertices:  # when not all vertices have been used
     vertexWithSmallestEdge = 0
     for vertex in usedVertices:
         while len(all_edges[vertex]) > 0 and all_edges[vertex][0][dest] in usedVertices:
