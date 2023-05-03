8783309282a7031ca109c92e4ea4cd2f8410c6c1
EnshenZhu <winniecmz.zhu@gmail.com>
2023-05-03 07:35:47 -0400

    Revert "reformat some scripts"
    This reverts commit 2f474b2c3f13a03fa11420327f9237dedcd523ef.

--- Dynamic_Programming/fibonacci/fib.py
+++ Dynamic_Programming/fibonacci/fib.py
@@ -1,9 +1,8 @@
 def fib(n):
-    if n <= 2:
+    if n<=2:
         return 1
     else:
-        return fib(n - 2) + fib(n - 1)
-
+        return fib(n-2)+fib(n-1)
 
 print(fib(6))
 print(fib(7))
--- ReverseLinkedList/src/ListNode.java
+++
@@ -1,9 +0,0 @@
-public class ListNode {
-    int val;
-    ListNode next;
-
-    public ListNode(int newVal, ListNode newNext) {
-        this.val=newVal;
-        this.next=newNext;
-    }
-}
